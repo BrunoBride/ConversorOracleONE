aea0c68ae5999c8a3a0f1d7948657f8090f1ad08
Bruno Bride <brunexcorp@gmail.com>
2023-07-27 15:27:00 -0300

    Inclui opções de moeda origem e opção moeda que será convertida (destino)

--- ConversorMoeda/src/ConversorMoeda.java
+++ ConversorMoeda/src/ConversorMoeda.java
@@ -1,35 +1,39 @@
-
 import java.util.Scanner;
 
 public class ConversorMoeda {
     public static void main(String[] args) {
-        // Definir a taxa de conversão das moedas
-        double taxaDolarParaReal = 5.35; // Exemplo: 1 dólar = 5.35 reais
-        
         // Obter o valor financeiro informado pelo usuário
         Scanner scanner = new Scanner(System.in);
         System.out.print("Digite o valor financeiro: ");
         double valorFinanceiro = scanner.nextDouble();
         
-        // Obter a moeda desejada para conversão
-        System.out.print("Digite a moeda para conversão (dólar, real, euro, etc.): ");
-        String moeda = scanner.next();
+        // Obter a moeda de origem
+        System.out.print("Digite a moeda de origem (dólar, real, euro, etc.): ");
+        String moedaOrigem = scanner.next();
+        
+        // Obter a moeda de conversão
+        System.out.print("Digite a moeda de conversão (dólar, real, euro, etc.): ");
+        String moedaConversao = scanner.next();
         
         // Realizar a conversão
-        double valorConvertido = 0.0;
-        
-        if (moeda.equalsIgnoreCase("dólar")) {
-            valorConvertido = valorFinanceiro * taxaDolarParaReal;
-        } else if (moeda.equalsIgnoreCase("real")) {
-            valorConvertido = valorFinanceiro / taxaDolarParaReal;
-        } else {
-            System.out.println("Moeda não suportada.");
-            return;
-        }
+        double valorConvertido = converterMoeda(valorFinanceiro, moedaOrigem, moedaConversao);
         
         // Exibir o resultado
-        System.out.println("Valor convertido: " + valorConvertido + " " + moeda);
+        System.out.println("Valor convertido: " + valorConvertido);
         
         scanner.close();
+    }
+    
+    public static double converterMoeda(double valor, String moedaOrigem, String moedaConversao) {
+        double taxaDolarParaReal = 5.35; // Exemplo: 1 dólar = 5.35 reais
+        
+        if (moedaOrigem.equalsIgnoreCase("dólar") && moedaConversao.equalsIgnoreCase("real")) {
+            return valor * taxaDolarParaReal;
+        } else if (moedaOrigem.equalsIgnoreCase("real") && moedaConversao.equalsIgnoreCase("dólar")) {
+            return valor / taxaDolarParaReal;
+        } else {
+            System.out.println("Conversão não suportada.");
+            return 0.0;
+        }
     }
 }
